2b42da3e0c9d74ff5d2ff042163d73cba74c5596
nattapatee <nattapat.eka@gmail.com>
2018-07-03 10:35:39 +0700

    3July

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
+import './index.css';
 
 
 type State = {
@@ -11,18 +13,75 @@ class App extends React.Component<{}, State> {
     constructor(props) {
         super(props);
         this.state = {
-            path: "/Users/wk",
+            path: "/Users/bcircle/src/SearchApi",
             pattern: "*.json"
         }
     }
 
     onClick = (e) => {
-        console.log("Hello, world!");
+        console.log(this.state)
+        var resultElement = document.getElementById('postResult');
+        resultElement.innerHTML = '';
+ 
+        axios.post('http://localhost:5002/api/search/searchFile', {
+        path: this.state.path, 
+        pattern: this.state.pattern,
+        headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+                    'Accept': 'application/json'
+      
+      }
+    
+      })
+      .then((response) => {
+        console.log(response.data)
+        resultElement.innerHTML = generateSuccessHTMLOutput(response);
+    })
+    .catch((error) => {
+        console.log("AXIOS ERROR: ", error)
+    })
+
+
+    function generateSuccessHTMLOutput(response) {
+        return  '<h5>Status:</h5>' +
+                '<pre>' + response.status + ' ' + response.statusText + '</pre>' +
+                '<h5>Headers:</h5>' +
+                '<pre>' + JSON.stringify(response.headers, null, '\t') + '</pre>' +
+                '<h5>Data:</h5>' +
+                '<pre>' + JSON.stringify(response.data, null, '\t') + '</pre>';
+      }
+        
+    
     }
 
-    render() {
+    
+
+    render()  
+    {
+        
+        
+        var divStyle = {
+        color: 'black',
+        backgroundColor: '#cccccc',
+        padding: "20px",
+        margin: "20px"
+        
+
+      };
+      
         return (
-            <h1 onClick={this.onClick}>Hello, world!</h1>
+            <div>
+                <h1>Search File</h1>
+                <div>
+                    <button onClick={this.onClick} >Search</button>
+                    <br /><br />
+                    Result :
+                    <div style={divStyle} id="postResult"></div>
+
+
+                </div>   
+            </div>       
+           
         )
     }
 }
