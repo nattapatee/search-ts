ca9233954d46b4136681ebd2e783ef1cb147c178
nattapatee <nattapat.eka@gmail.com>
2018-07-03 16:52:42 +0700

    Add Input for users

--- src/index.tsx
+++ src/index.tsx
@@ -13,15 +13,32 @@ class App extends React.Component<{}, State> {
     constructor(props) {
         super(props);
         this.state = {
-            path: "/Users/bcircle/src/SearchApi",
-            pattern: "*.json"
+            path: "",
+            pattern: ""
         }
+
+        this.handlepathChange = this.handlepathChange.bind(this)
+        this.handlepatternChange = this.handlepatternChange.bind(this)
     }
+
+    handlepathChange (evt) {
+
+        this.setState({ path: evt.target.value })
+      }
+      
+      handlepatternChange (evt) {
+        
+        this.setState({ pattern: evt.target.value })
+      }
+    
+    
+
+
 
     onClick = (e) => {
         console.log(this.state)
-        let resultElement = document.getElementById('postResult');
-        resultElement.innerHTML = '';
+        let resultElement = document.getElementById('postResult')
+        resultElement.innerHTML = ''
  
         axios.post('http://localhost:5002/api/search/searchFile', {
         path: this.state.path, 
@@ -35,7 +52,7 @@ class App extends React.Component<{}, State> {
       })
       .then((response) => {
         console.log(response.data)
-        resultElement.innerHTML = generateSuccessHTMLOutput(response);
+        resultElement.innerHTML = generateSuccessHTMLOutput(response)
     })
     .catch((error) => {
         console.log("AXIOS ERROR: ", error)
@@ -48,30 +65,37 @@ class App extends React.Component<{}, State> {
                 '<h5>Headers:</h5>' +
                 '<pre>' + JSON.stringify(response.headers, null, '\t') + '</pre>' +
                 '<h5>Data:</h5>' +
-                '<pre>' + JSON.stringify(response.data, null, '\t') + '</pre>';
+                '<pre>' + JSON.stringify(response.data, null, '\t') + '</pre>'
       }
         
     
     }
 
-    
-
     render()  
     {
-
         var divStyle = {
         color: 'black',
         backgroundColor: '#cccccc',
         padding: "20px",
         margin: "20px"
-        
-
       };
       
         return (
             <div>
-                <h1>Search File</h1>
+                <h1>Search File[TS ver.]</h1>
                 <div>
+
+                <div className="text">
+                    <label>Path :   </label>
+                    <input type="text" name="path" placeholder="Todo Path ..." onChange={this.handlepathChange}></input>
+                </div>
+
+                <div className="text">
+                    <label>Pattern :    </label>
+                    <input type="text" name="pattern"  placeholder="Todo Pattern ..." onChange={this.handlepatternChange}></input>
+                
+                </div>
+
                     <button onClick={this.onClick} >Search</button>
                     <br /><br />
                     Result :
@@ -86,4 +110,4 @@ class App extends React.Component<{}, State> {
 }
 
 var root = document.getElementById("root")
-ReactDOM.render(<App />, root);
+ReactDOM.render(<App />, root)
